5af9e1cfd3789df95fd4f371a4e386ecf4369a92
SVlad19 <mr.vladik.semeno3@gmail.com>
2023-04-03 21:40:51 +0300

    Add new Activity(ChangeUsername)

---
+++ app/src/main/java/com/example/lab21foroop/ChangeUsername.kt
@@ -0,0 +1,11 @@
+package com.example.lab21foroop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChangeUsername : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_username)
+    }
+}
--- app/src/main/java/com/example/lab21foroop/MainActivity.kt
+++ app/src/main/java/com/example/lab21foroop/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.lab21foroop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.lab21foroop.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+
     }
 }
