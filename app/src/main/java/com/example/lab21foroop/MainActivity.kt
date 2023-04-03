7e3255916541da15bba7cc38b09133d11123a1ee
SVlad19 <mr.vladik.semeno3@gmail.com>
2023-04-03 23:04:34 +0300

    Added layer for authorization and photo selection from gallery

--- app/src/main/java/com/example/lab21foroop/MainActivity.kt
+++ app/src/main/java/com/example/lab21foroop/MainActivity.kt
@@ -4,12 +4,17 @@ import android.app.DownloadManager.Request
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
 import com.example.lab21foroop.databinding.ActivityChangeUsernameBinding
 import com.example.lab21foroop.databinding.ActivityMainBinding
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val REQUEST_CODE = 3
+    private val REQUEST_CODE_FOR_NICKNAME = 3
+    private val REQUEST_CODE_FOR_IMAGE = 2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -23,13 +28,41 @@ class MainActivity : AppCompatActivity() {
             startActivityForResult(intent,3)
         }
 
+        binding.btnLogIn.setOnClickListener(){
+            binding.TVUserName.setText(binding.edLogin.text.toString())
+            binding.llFirstLayout.visibility = View.GONE
+        }
+
+        binding.IVMainPhoto.setOnClickListener(){
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(intent, REQUEST_CODE_FOR_IMAGE)
+
+
+        }
+
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == REQUEST_CODE){
-            binding.TVUserName.setText(data?.getStringExtra("Username"))
+
+        when(requestCode){
+            REQUEST_CODE_FOR_NICKNAME ->
+            {
+                binding.TVUserName.setText(data?.getStringExtra("Username"))
+            }
+
+            REQUEST_CODE_FOR_IMAGE->
+            {
+                val imageUri = data?.data
+                val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, imageUri)
+                binding.IVMainPhoto.setImageBitmap(bitmap)
+            }
+            else ->{
+                finishAffinity()
+            }
         }
+
+
 
     }
 }
