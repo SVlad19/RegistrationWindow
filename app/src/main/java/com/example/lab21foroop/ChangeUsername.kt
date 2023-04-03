6d8c715c9fa4b55abb7508b9cd5978387f03ed3e
SVlad19 <mr.vladik.semeno3@gmail.com>
2023-04-03 22:27:09 +0300

    Added ability to change nickname

--- app/src/main/java/com/example/lab21foroop/ChangeUsername.kt
+++ app/src/main/java/com/example/lab21foroop/ChangeUsername.kt
@@ -1,11 +1,26 @@
 package com.example.lab21foroop
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.lab21foroop.databinding.ActivityChangeUsernameBinding
 
 class ChangeUsername : AppCompatActivity() {
+    private lateinit var binding: ActivityChangeUsernameBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_change_username)
+        binding = ActivityChangeUsernameBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.btnOk.setOnClickListener(){
+            val resultIntent = Intent(this,MainActivity::class.java)
+            resultIntent.putExtra("Username",binding.etNewnickname.text.toString())
+            setResult(Activity.RESULT_OK,resultIntent)
+            finish()
+        }
+
     }
 }
--- app/src/main/java/com/example/lab21foroop/MainActivity.kt
+++ app/src/main/java/com/example/lab21foroop/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.lab21foroop
 
+import android.app.DownloadManager.Request
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.lab21foroop.databinding.ActivityChangeUsernameBinding
 import com.example.lab21foroop.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val REQUEST_CODE = 3
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -13,6 +17,19 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
+        val intent = Intent(this,ChangeUsername::class.java)
+
+        binding.TVUserName.setOnClickListener(){
+            startActivityForResult(intent,3)
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == REQUEST_CODE){
+            binding.TVUserName.setText(data?.getStringExtra("Username"))
+        }
 
     }
 }
